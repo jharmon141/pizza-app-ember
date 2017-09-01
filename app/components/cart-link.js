a7dcea9a4ec377342efabe1a607b1dede7ddff0c
jharmon141 <jeremy.l.harmon@gmail.com>
2017-08-31 21:40:21 -0500

    bug fixed

--- app/components/cart-link.js
+++ app/components/cart-link.js
@@ -2,10 +2,4 @@ import Ember from 'ember';
 
 export default Ember.Component.extend({
   cart: Ember.inject.service(),
-
-  actions: {
-    handleResetForm() {
-      this.sendAction('resetForm');
-    }
-  }
 });
